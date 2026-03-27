6e55bc3a3cebf5d38d6844af0fce44352e2eee70
Bruno Bacelar Abe <abe2602@gmail.com>
2026-03-27 12:03:18 -0300

    [NO-TASK] Add intl, bloc, cubit and rxdart

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'Moowvies';
+
+  @override
+  String get homeTitle => 'Moowvies';
+
+  @override
+  String get counterLabel => 'You have pushed the button this many times:';
+
+  @override
+  String get increment => 'Increment';
+}
