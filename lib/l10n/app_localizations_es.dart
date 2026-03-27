5372fc7d9a29c0bc9992b70cf091d3934929cccb
Bruno Bacelar Abe <abe2602@gmail.com>
2026-03-27 12:03:18 -0300

    [NO-TASK] Add intl, bloc, cubit and rxdart

---
+++ lib/l10n/app_localizations_es.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get appTitle => 'Moowvies';
+
+  @override
+  String get homeTitle => 'Moowvies';
+
+  @override
+  String get counterLabel => 'Has pulsado el botón esta cantidad de veces:';
+
+  @override
+  String get increment => 'Incrementar';
+}
---
+++ lib/l10n/app_localizations_pt.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Portuguese (`pt`).
+class AppLocalizationsPt extends AppLocalizations {
+  AppLocalizationsPt([String locale = 'pt']) : super(locale);
+
+  @override
+  String get appTitle => 'Moowvies';
+
+  @override
+  String get homeTitle => 'Moowvies';
+
+  @override
+  String get counterLabel => 'Você pressionou o botão este número de vezes:';
+
+  @override
+  String get increment => 'Incrementar';
+}
