db0434c6c1e0c31d74405168a83b8da5697da66c
Bruno Bacelar Abe <abe2602@gmail.com>
2026-03-31 21:55:37 -0300

    [NO-TASK] Add home screen

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,13 +1,50 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:home/home_router.dart';
+import 'package:moowvie/routes/main_screen.dart';
 import 'package:movies_list/movies_list_router.dart';
+import 'package:reviews/reviews_router.dart';
+import 'package:search/search_router.dart';
+
+part 'app_router.gr.dart';
 
 @AutoRouterConfig()
 class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
         AutoRoute(
-          page: MoviesListRoute.page,
+          page: MainRoute.page,
           initial: true,
+          children: [
+            AutoRoute(
+              page: HomeTab.page,
+              children: [
+                AutoRoute(
+                  page: HomeRoute.page,
+                  initial: true,
+                  children: [
+                    AutoRoute(page: MoviesListRoute.page, initial: true),
+                    AutoRoute(page: ReviewsRoute.page),
+                  ],
+                ),
+              ],
+            ),
+            AutoRoute(
+              page: SearchTab.page,
+              children: [
+                AutoRoute(page: SearchRoute.page, initial: true),
+              ],
+            ),
+          ],
         ),
       ];
+}
+
+@RoutePage(name: 'HomeTab')
+class HomeTabPage extends AutoRouter {
+  const HomeTabPage({super.key});
+}
+
+@RoutePage(name: 'SearchTab')
+class SearchTabPage extends AutoRouter {
+  const SearchTabPage({super.key});
 }
