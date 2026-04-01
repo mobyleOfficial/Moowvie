35589eac925e10f289839f4860f5228d70993840
Bruno Bacelar Abe <abe2602@gmail.com>
2026-03-31 23:20:38 -0300

    [NO-TASK] Fix black screen

--- lib/routes/main_screen.dart
+++ lib/routes/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:moovie/routes/app_router.dart';
+import 'package:profile_ui/profile_router.dart';
 
 @RoutePage()
 class MainScreen extends StatelessWidget {
@@ -16,6 +17,18 @@ class MainScreen extends StatelessWidget {
       builder: (context, child) {
         final tabsRouter = AutoTabsRouter.of(context);
         return Scaffold(
+          appBar: AppBar(
+            title: const Text('Moovie'),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.person_outline),
+                onPressed: () {
+                  context.router.root.push(const ProfileRoute());
+                },
+              ),
+            ],
+          ),
           body: SafeArea(child: child),
           bottomNavigationBar: BottomNavigationBar(
             currentIndex: tabsRouter.activeIndex,
