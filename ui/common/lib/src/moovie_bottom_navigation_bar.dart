724f2b601dc1e36d064aef2b76f605a99306e77f
Bruno Abe <abe2602@gmail.com>
2026-04-04 09:28:10 -0300

    [ios-fixes] Fix bottom nav

--- ui/common/lib/src/moovie_bottom_navigation_bar.dart
+++ ui/common/lib/src/moovie_bottom_navigation_bar.dart
@@ -1,8 +1,4 @@
-import 'package:common/src/theme/moovie_colors.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import 'package:common/src/platform_helper.dart';
 
 class MoovieBottomNavigationBarItem {
   final IconData icon;
@@ -20,46 +16,98 @@ class MoovieBottomNavigationBar extends StatelessWidget {
   final int currentIndex;
   final ValueChanged<int> onTap;
   final List<MoovieBottomNavigationBarItem> items;
+  final MoovieBottomNavigationBarItem? centerItem;
+  final VoidCallback? onCenterTap;
 
   const MoovieBottomNavigationBar({
     super.key,
     required this.currentIndex,
     required this.onTap,
     required this.items,
+    this.centerItem,
+    this.onCenterTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    if (isIOS) {
-      return CupertinoTabBar(
-        currentIndex: currentIndex,
-        onTap: onTap,
-        activeColor: MoovieColors.secondary,
-        items: items
+    final hasCenter = centerItem != null;
+
+    if (!hasCenter) {
+      return NavigationBar(
+        selectedIndex: currentIndex,
+        onDestinationSelected: onTap,
+        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        destinations: items
             .map(
-              (item) => BottomNavigationBarItem(
-                icon: Icon(item.icon),
-                activeIcon: Icon(item.activeIcon ?? item.icon),
-                label: item.label,
-              ),
-            )
+              (item) => NavigationDestination(
+            icon: Icon(item.icon),
+            selectedIcon: Icon(item.activeIcon ?? item.icon),
+            label: item.label,
+          ),
+        )
             .toList(),
       );
     }
 
-    return NavigationBar(
-      selectedIndex: currentIndex,
-      onDestinationSelected: onTap,
-      labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-      destinations: items
-          .map(
-            (item) => NavigationDestination(
-              icon: Icon(item.icon),
-              selectedIcon: Icon(item.activeIcon ?? item.icon),
-              label: item.label,
+    final colorScheme = Theme.of(context).colorScheme;
+    final centerIndex = items.length ~/ 2;
+    final mappedIndex =
+    currentIndex >= centerIndex ? currentIndex + 1 : currentIndex;
+
+    final destinations = <NavigationDestination>[];
+    for (var i = 0; i < items.length; i++) {
+      if (i == centerIndex) {
+        destinations.add(
+          NavigationDestination(
+            icon: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+              decoration: BoxDecoration(
+                color: colorScheme.secondary,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Icon(centerItem!.icon, color: colorScheme.onSecondary),
             ),
-          )
-          .toList(),
+            label: centerItem!.label,
+          ),
+        );
+      }
+      final item = items[i];
+      destinations.add(
+        NavigationDestination(
+          icon: Icon(item.icon),
+          selectedIcon: Icon(item.activeIcon ?? item.icon),
+          label: item.label,
+        ),
+      );
+    }
+
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.topCenter,
+      children: [
+        NavigationBar(
+          selectedIndex: mappedIndex,
+          onDestinationSelected: (index) {
+            if (index == centerIndex) {
+              onCenterTap?.call();
+              return;
+            }
+            onTap(index > centerIndex ? index - 1 : index);
+          },
+          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+          destinations: destinations,
+        ),
+        Positioned(
+          top: 8,
+          child: FloatingActionButton(
+            onPressed: onCenterTap,
+            tooltip: centerItem!.label,
+            backgroundColor: colorScheme.secondary,
+            foregroundColor: colorScheme.onSecondary,
+            child: Icon(centerItem!.icon, semanticLabel: ''),
+          ),
+        ),
+      ],
     );
   }
 }
