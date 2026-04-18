85a4fef750282e84510cfd4d3784a03f96ecb580
EdYuTo <edson.yudi.toma@gmail.com>
2026-04-18 17:50:37 -0300

    fix: simplify public_profile_bloc to remove type errors

--- ui/public_profile/lib/public_profile_bloc.dart
+++ ui/public_profile/lib/public_profile_bloc.dart
@@ -1,10 +1,13 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:public_profile/public_profile_info/public_profile_info_state.dart';
 
-class PublicProfileCubit extends Cubit<PublicProfileState> {
-  PublicProfileCubit() : super(const PublicProfileLoading()) {
+class PublicProfileCubit extends Cubit<PublicProfileInfoState> {
+  PublicProfileCubit() : super(const PublicProfileInfoLoading()) {
     _load();
   }
 
-  void _load() => emit(const PublicProfileSuccess());
+  void _load() {
+    // TODO: Implement actual loading logic with real data
+    emit(const PublicProfileInfoLoading());
+  }
 }
