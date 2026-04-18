9575be6f8e7276fbaa08c48642adc259b677c088
EdYuTo <edson.yudi.toma@gmail.com>
2026-04-18 17:49:49 -0300

    fix: correct public_profile_bloc import path

--- ui/public_profile/lib/public_profile_bloc.dart
+++ ui/public_profile/lib/public_profile_bloc.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:public_profile/public_profile_info/public_profile_state.dart';
+import 'package:public_profile/public_profile_info/public_profile_info_state.dart';
 
 class PublicProfileCubit extends Cubit<PublicProfileState> {
   PublicProfileCubit() : super(const PublicProfileLoading()) {
